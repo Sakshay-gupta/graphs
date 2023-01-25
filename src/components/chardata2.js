d5dbeeab23fa202ad8ac8e9c596afe5e5238ef68
Sakshay-gupta <75110589+Sakshay-gupta@users.noreply.github.com>
2023-01-25 15:26:35 +0530

    modified:   src/components/chardata2.js

--- src/components/chardata2.js
+++ src/components/chardata2.js
@@ -31,14 +31,35 @@ const ChartData2 = ({data, param}) => {
         setcsvData(data, param)
     }
 
-    const setXaxis = (data) =>{
+    const setXaxis = (data) => {
+        let temp1 = []
         let cate = []
         data[0].map(item => {
-            let ind = cate.findIndex(x => x === item['L2 Cluster'])
-            if(ind < 0){
-                cate.push(item['L2 Cluster'])
-            }
+                let ind = temp1.findIndex(x => x === item['L2 Cluster'])
+                if(ind < 0) temp1.push(item['L2 Cluster'])
         })
+        let temp = []
+        temp1.map(item => {
+            let sum = 0
+            data.map(item2 => {
+                let ind = item2.findIndex(x => (x['L2 Cluster'] === item && x['Sentiment'] === 'Positive'))
+                if(ind >= 0){
+                    sum += parseInt(item2[ind]['L2 Review Count'])
+                }
+                let ind2 = item2.findIndex(x => (x['L2 Cluster'] === item && x['Sentiment'] === 'Negative'))
+                if(ind2 >= 0){
+                    sum += parseInt(item2[ind2]['L2 Review Count'])
+                }
+            })
+            temp.push({
+                l2:item,
+                count:sum
+            })
+        })
+        temp.sort((a, b) => b.count - a.count)
+        for(var i = 0; i < 10; i++){
+            cate.push(temp[i]['l2'])
+        }
         return cate;
     }
     
