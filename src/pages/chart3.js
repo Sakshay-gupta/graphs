6a7fac27e03fb529d35b7673fc7d6b5b65204c21
Sakshay-gupta <sakshaygoyal@gmail.com>
2023-03-09 04:38:26 +0530

    Checking

--- src/pages/chart3.js
+++ src/pages/chart3.js
@@ -1,94 +1,95 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import HeaderChart from "../components/headerchart";
 import PieChartData from "../components/piechartdata";
 import { useLocation } from "react-router";
 import Select from "react-select";
 
-
 const ops = [
-    {
-        label:"Top 5",
-        value:5
-    },
-    {
-        label:"Top 10",
-        value:10
-    },
-    {
-        label:"Top 15",
-        value:15
-    },
-    {
-        label:"Top 20",
-        value:20,
-    }
-]
+  {
+    label: "Top 5",
+    value: 5,
+  },
+  {
+    label: "Top 10",
+    value: 10,
+  },
+  {
+    label: "Top 15",
+    value: 15,
+  },
+  {
+    label: "Top 20",
+    value: 20,
+  },
+];
 const Chart3 = () => {
-    const location = useLocation();
-    const {chartData} = location.state
-    const [labels, setLabels] = useState(null)
-    const [sortBy, setSort] = useState(10)
-    //const [flow, setFlow] = useState(false)
-    useEffect(() => {
-        setLabels(setXaxispre(chartData, 10))
-    }, [])
-    const handleChangeSort = selectedOption => {
-        const temp = setXaxispre(chartData, selectedOption.value)
-        setLabels(temp)
-        
-    }
-    // const handleFilter = () => {
-    //     setFlow(false)
-    //     console.log(sortBy)
-    //     setLabels(setXaxispre(chartData, sortBy))
-    //     console.log(labels)
-    // }
+  const location = useLocation();
+  const { chartData } = location.state;
+  const [labels, setLabels] = useState(null);
+  const [sortBy, setSort] = useState(10);
+  //const [flow, setFlow] = useState(false)
+  useEffect(() => {
+    setLabels(setXaxispre(chartData, 10));
+  }, []);
+  const handleChangeSort = (selectedOption) => {
+    const temp = setXaxispre(chartData, selectedOption.value);
+    setLabels(temp);
+  };
+  // const handleFilter = () => {
+  //     setFlow(false)
+  //     console.log(sortBy)
+  //     setLabels(setXaxispre(chartData, sortBy))
+  //     console.log(labels)
+  // }
 
-    const setXaxispre = (data, sortBy) => {
-        let temp1 = []
-        let cate = []
-        data[0].map(item => {
-                let ind = temp1.findIndex(x => x === item['L2 Cluster'])
-                if(ind < 0) temp1.push(item['L2 Cluster'])
-        })
-        let temp = []
-        temp1.map(item => {
-            let sum = 0
-            data.map(item2 => {
-                let ind = item2.findIndex(x => (x['L2 Cluster'] === item && x['Sentiment'] === 'Positive'))
-                if(ind >= 0){
-                    sum += parseInt(item2[ind]['L2 Review Count'])
-                }
-                let ind2 = item2.findIndex(x => (x['L2 Cluster'] === item && x['Sentiment'] === 'Negative'))
-                if(ind2 >= 0){
-                    sum += parseInt(item2[ind2]['L2 Review Count'])
-                }
-            })
-            temp.push({
-                l2:item,
-                count:sum
-            })
-        })
-        temp.sort((a, b) => b.count - a.count)
-        for(var i = 0; i < (temp.length < sortBy ? temp.length : sortBy); i++){
-            cate.push(temp[i]['l2'])
+  const setXaxispre = (data, sortBy) => {
+    let temp1 = [];
+    let cate = [];
+    data[0].map((item) => {
+      let ind = temp1.findIndex((x) => x === item["L2 Cluster"]);
+      if (ind < 0) temp1.push(item["L2 Cluster"]);
+    });
+    let temp = [];
+    temp1.map((item) => {
+      let sum = 0;
+      data.map((item2) => {
+        let ind = item2.findIndex(
+          (x) => x["L2 Cluster"] === item && x["Sentiment"] === "Positive"
+        );
+        if (ind >= 0) {
+          sum += parseInt(item2[ind]["L2 Review Count"]);
         }
-        return cate;
+        let ind2 = item2.findIndex(
+          (x) => x["L2 Cluster"] === item && x["Sentiment"] === "Negative"
+        );
+        if (ind2 >= 0) {
+          sum += parseInt(item2[ind2]["L2 Review Count"]);
+        }
+      });
+      temp.push({
+        l2: item,
+        count: sum,
+      });
+    });
+    temp.sort((a, b) => b.count - a.count);
+    for (var i = 0; i < (temp.length < sortBy ? temp.length : sortBy); i++) {
+      cate.push(temp[i]["l2"]);
     }
-    return(<>
-        <HeaderChart chartData={chartData}/>
-        <div className="app_container">
-        <div style={{display:"flex", marginBottom:"10px"}}>
-            <div style={{width:"40%"}}>
-                <Select
-                    options={ops}
-                    onChange={handleChangeSort}
-                    />
-            </div>
+    return cate;
+  };
+  return (
+    <>
+      <HeaderChart chartData={chartData} />
+      <div className="app_container">
+        <div style={{ display: "flex", marginBottom: "10px" }}>
+          <div style={{ width: "40%" }}>
+            <Select options={ops} onChange={handleChangeSort} />
+          </div>
         </div>
-        {labels ? <PieChartData data={chartData} labels={labels}/> : null}
-        </div>
-    </>)
-}
+        {labels ? <PieChartData data={chartData} labels={labels} /> : null}
+      </div>
+    </>
+  );
+};
 
 export default Chart3;
